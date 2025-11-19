e3c3d590f3ee4932a89c7accc2d82ea055cd0a83
felipelovis <vasconceloslovison@gmail.com>
2025-11-18 23:32:19 -0300

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ const POWER_BI_LINKS = {
 
 // DATAS LIMITE por fase (CORRIGIDO PARA 2026!)
 const DATAS_LIMITE = {
-    'Grupo': new Date('2025-11-12T14:00:00'),
+    'Grupo': new Date('2026-06-12T14:00:00'),
     '16 avos': new Date('2025-11-15T12:00:00'),
     'Oitavas de final': new Date('2025-11-12T12:00:00'),
     'Quartas de final': new Date('2025-11-12T16:00:00'),
